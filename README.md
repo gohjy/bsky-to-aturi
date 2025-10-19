9ed178d340df58df951bf73a7a241502c185fc56
gohjy <154421097+gohjy@users.noreply.github.com>
2025-10-19 11:30:13 +0000

    v0.2.0

--- README.md
+++ README.md
@@ -1,46 +1,53 @@
 # bsky-to-aturi
-Convert a Bluesky post URL to an AT URI in JavaScript. Uses the Bluesky public API to resolve handles to their DID when needed.
+Convert a Bluesky post URL to an AT URI in JavaScript. Uses the Bluesky public API (`public.api.bsky.app`) to resolve handles to their DID when needed.
 
 The function is implemented as an **`async` function**, because it may need to make a request to Bluesky's API to resolve a handle to its DID.
-
-**Disclaimer**: This will almost certainly break. There is near-zero error handling present in the scripts. Use with caution. (Or just use the online tool below if you can and save the URI.)
 
 ## Demo / online tool
 <https://gohjy.github.io/bsky-to-aturi>
 
 ## Usage
-### ES Module
-In your JavaScript (module):
+In your JavaScript (only ES module script is provided):
 ```js
-import bskyToAtUri from "https://cdn.jsdelivr.net/gh/gohjy/bsky-to-aturi@0.1.0/bsky-to-aturi.mjs";
+import bskyToAtUri from "https://cdn.jsdelivr.net/gh/gohjy/bsky-to-aturi@0.2.0/bsky-to-aturi.mjs";
 
 // Later...
 try {
-    const atUri = await bskyToAtUri(bskyPostUrlFromSomewhere);
-    if (!atUri) throw new Error();
-    // do stuff with URI here
-} catch(e) {
-    console.error(e);
-    console.log("No AT URI found, check your Bluesky post URL");
+    const atUri = await bskyToAtUri(bskyPostUri);
+    // do stuff with URI
+} catch({ error, message }) {
+    // error handling
+    console.error(`Something went wrong: ${error}: ${message}`);
+}
+
+// alternatively, if you don't need error handling:
+const uri = await bskyToAtUri(bskyPostUrl).catch(() => null);
+if (!uri) {
+    // something went wrong
+} else {
+    // do stuff with URI
 }
 ```
 
-### Non-modular
-Include the script from this URL:
-```plain
-https://cdn.jsdelivr.net/gh/gohjy/bsky-to-aturi@0.1.0/bsky-to-aturi.js
-```
-This will define a `bskyToAtUri` property on `globalThis`:
-```js
-bskyToAtUri(bskyPostUrl)
-.then(uri => {
-    if (!uri) throw new Error();
-    // do stuff with URI here
-})
-.catch(() => {
-    console.log("No AT URI found. Check your Bluesky post URL.");
-})
-```
+## Documentation
+Pass a string to the function in one of the following forms:
+- `https://bsky.app/profile/{handleOrDid}`
+- `https://bsky.app/profile/{handleOrDid}/post/{postId}`
+
+The hostname need not be `bsky.app`, any syntactically valid hostname will be accepted.
+
+The function will return a `Promise`.
+
+### Scenario 1: everything went fine
+The Promise resolves to a string, which is the AT URI.
+
+### Scenario 2: something went wrong
+The Promise will reject with an object. The `error` and `message` properties will contain more information. The `error` property is intended for code to distinguish between errors, while the `message` property is a human-readable message.
+
+Possible `error` values include:
+- `InvalidUrl`: invalid Bluesky URL provided
+- `FetchError`: `fetch()` failed. Most likely a network error.
+- `ServerError`: Bluesky API returned an invalid response (invalid JSON)
 
 ## License
-[MIT](./LICENSE) ([view on choosealicense.com](https://choosealicense.com/licenses/mit/))
+[MIT License](./LICENSE) ([view on choosealicense.com](https://choosealicense.com/licenses/mit/))
--- bsky-to-aturi.mjs
+++ bsky-to-aturi.mjs
@@ -1,21 +1,95 @@
-const bskyToAtUri = async (bskyUri) => {
-    if (!bskyUri.match(
-        /^https\:\/\/bsky\.app\/profile\/(did\:[a-z]+\:[a-zA-Z0-9\._\:\%\-]*[a-zA-Z0-9\._\-])|(([a-zA-Z0-9\-]{1,63}\.)+[a-zA-Z][a-zA-Z0-9\-]+)\/post\/[A-Za-z0-9\._\:\~\-]+$/
-    )) return null;
+async function resolveHandle(handle) {
+    const fetchUrl = new URL("https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle");
+    fetchUrl.searchParams.set("handle", handle);
+    const fetchResult = await fetch(fetchUrl.href).catch(() => ({
+        error: "FetchError",
+        message: "Failed to fetch Bluesky API"
+    }));
+    if (fetchResult.error) return fetchResult;
 
-    const urlObj = new URL(bskyUri);
-    const urlified = urlObj.href;
-
-    let did = urlified.match(/(?<=^https\:\/\/bsky\.app\/profile\/)[^\/]+(?=\/post\/[A-Za-z0-9\._\:\~\-]+$)/)[0];
-    if (!did.match(/did\:[a-z]+\:[a-zA-Z0-9\._\:\%\-]*[a-zA-Z0-9\._\-]/)) {
-        const reqUrl = new URL("https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle");
-        reqUrl.searchParams.set("handle", did);
-        did = await (await fetch(reqUrl)).json();
-        if (did.error) return null;        
-        did = did.did;
-    }
-
-    return `at://${did}/app.bsky.feed.post/${urlified.split("/").pop()}`;
+    const json = await fetchResult.json().catch(() => ({
+        error: "ServerError",
+        message: "Bluesky API did not return valid JSON"
+    }));
+    
+    if (json.did) return json.did;
+    return json;
 }
 
-export {bskyToAtUri as default};
+const REGEX = {
+    // https://atproto.com/specs/did#at-protocol-did-identifier-syntax
+    DID: /^did:[a-z]+:[a-zA-Z0-9._:%-]*[a-zA-Z0-9._-]$/,
+
+    // https://atproto.com/specs/handle#handle-identifier-syntax
+    HANDLE: /^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$/,
+
+    // https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/feed/post.json#L8 specifies app.bsky.feed.post records must have TID rkey
+    // https://atproto.com/specs/tid#tid-syntax
+    TID: /^[234567abcdefghij][234567abcdefghijklmnopqrstuvwxyz]{12}$/
+};
+
+function destructureBskyUrl(bskyUrl) {
+    const urlObj = (() => {
+        try {
+            return new URL(bskyUrl);
+        } catch {
+            return null;
+        }
+    })();
+    if (!urlObj) return null;
+
+    const pathname = urlObj.pathname;
+
+    const [_fullMatch, handleOrDid, rkey] = pathname.match(/^\/profile\/([^\/]+)(?:\/(?:post\/([^\/]+)\/?)?)?$/) || [null];
+
+    if (!_fullMatch) return null;
+    if (rkey && !rkey.match(REGEX.TID)) return null;
+
+    if (handleOrDid.match(REGEX.HANDLE)) {
+        return {
+            type: "handle", 
+            repo: handleOrDid,
+            rkey
+        };
+    } else if (handleOrDid.match(REGEX.DID)) {
+        return {
+            type: "did",
+            repo: handleOrDid,
+            rkey
+        };
+    }
+
+    return null;
+}
+
+function constructUri({repo, rkey}) {
+    if (!rkey) {
+        // default to app.bsky.actor.profile
+        return `at://${repo}/app.bsky.actor.profile/self`;
+    } 
+    return `at://${repo}/app.bsky.feed.post/${rkey}`;
+}
+
+export default async function(bskyUrl) {
+    const urlComponents = destructureBskyUrl(bskyUrl);
+    if (!urlComponents) throw {
+        error: "InvalidUrl",
+        message: "Invalid Bluesky URL provided"
+    };
+
+    if (urlComponents.type === "did") {
+        // just construct URI and return
+        return constructUri(urlComponents);
+    } else {
+        // it's a handle
+        // 1. resolve handle
+        const did = await resolveHandle(urlComponents.repo);
+        if (did.error) throw did;
+
+        // 2. construct URI and return
+        return constructUri({
+            repo: did,
+            rkey: urlComponents.rkey
+        });
+    }
+}
